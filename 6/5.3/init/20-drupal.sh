699d3f9562998726a1acbf4be4ed5a74fc169b7b
Chingis Sandanov <chingis@wodby.com>
2017-04-13 21:30:23 +0700

    Update db settings

--- 6/5.3/init/20-drupal.sh
+++ 6/5.3/init/20-drupal.sh
@@ -13,22 +13,7 @@ if ! [ -e ./web/index.php ]; then
     echo >&2 "Complete! Drupal has been successfully copied to ${APP_ROOT}"
 
     if [[ -z "${DRUPAL_VERSION}" ]]; then
-        settings=$(cat <<'END_HEREDOC'
-<?php
-
-$db_settings = array(
-  'host' => getenv('DB_HOST'),
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USERNAME'),
-  'password' => getenv('DB_PASSWORD'),
-  'driver' => getenv('DB_DRIVER'),
-);
-
-$db_url = strtr('driver://username:password@host/database', $db_settings);
-END_HEREDOC
-)
-
-        echo "${settings}" > "${APP_ROOT}/web/sites/default/settings.php"
-        chown www-data:www-data "${APP_ROOT}/web/sites/default/settings.php"
+        cd "${APP_ROOT}/web/sites/default"
+        su-exec www-data cp "default.settings.php" "settings.php"
     fi
 fi
