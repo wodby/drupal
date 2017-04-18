dbe11658144957000891c21832975a34954d0678
Chingis Sandanov <chingis@wodby.com>
2017-04-18 22:48:30 +0700

    Fix drupal 6 images

--- 6/5.3/init/20-drupal.sh
+++ 6/5.3/init/20-drupal.sh
@@ -6,15 +6,14 @@ if [[ ! -z "${DEBUG}" ]]; then
     set -x
 fi
 
-if ! [ -e ./web/index.php ]; then
+if ! [ -e ./index.php ]; then
     echo >&2 "Drupal not found in ${APP_ROOT} - copying now..."
     chown -R www-data:www-data "${APP_ROOT}"
-    su-exec www-data mkdir -p web
-    su-exec www-data rsync -rlt "/usr/src/drupal/" "${APP_ROOT}/web"
+    su-exec www-data rsync -rlt "/usr/src/drupal/" "${APP_ROOT}"
     echo >&2 "Complete! Drupal has been successfully copied to ${APP_ROOT}"
 
     if [[ -z "${DRUPAL_VERSION}" ]]; then
-        cd "${APP_ROOT}/web/sites/default"
+        cd "${APP_ROOT}/sites/default"
         su-exec www-data cp "default.settings.php" "settings.php"
     fi
 fi
--- 6/5.3/test.sh
+++ 6/5.3/test.sh
@@ -11,7 +11,7 @@ make start
 echo "Wait for Drupal to be copied"
 sleep 3
 echo -n "Checking Drupal version... "
-docker exec --user=82 "${NAME}" drush -r /var/www/html/web status | grep -q 'Drupal version *: *6\.38'
+docker exec --user=82 "${NAME}" drush -r /var/www/html/ status | grep -q 'Drupal version *: *6\.38'
 echo "OK"
 
 make clean
