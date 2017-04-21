a0d1d0ccea8dafa4349995b4f419bfb9699855e7
Pavel Prischepa <pprischepa@gmail.com>
2017-04-21 09:41:48 +0700

    Fixed readiness check

--- 7/7.0/actions/wait-for-drupal-copy.sh
+++ 7/7.0/actions/wait-for-drupal-copy.sh
@@ -6,7 +6,20 @@ if [[ -n "${DEBUG}" ]]; then
   set -x
 fi
 
-if [[ ! -f "${APP_ROOT}/.ready" ]]; then
+ready=0
+max_try=$1
+wait_seconds=$2
+
+for i in $(seq 1 "${max_try}"); do
+    if [[ -f "${APP_ROOT}/.ready" ]]; then
+        ready=1
+        break
+    fi
+    echo 'Drupal is copying...'
+    sleep "${wait_seconds}"
+done
+
+if [[ "${ready}" -eq '0' ]]; then
     echo >&2 'Error. Failed to copy Drupal.'
     exit 1
 fi
