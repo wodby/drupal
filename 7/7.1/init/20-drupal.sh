5a0cbfe720105ca6572885d701900638b3cfb096
Chingis Sandanov <chingis@wodby.com>
2017-04-24 18:50:30 +0700

    get rid of auto init env var

--- 7/7.1/init/20-drupal.sh
+++ 7/7.1/init/20-drupal.sh
@@ -6,6 +6,6 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-if [[ -z "${DRUPAL_VERSION}" || -n "${DRUPAL_AUTO_INIT}" ]]; then
+if [[ -z "${DRUPAL_VERSION}" ]]; then
     su-exec www-data make init -f /usr/local/bin/actions.mk
 fi
