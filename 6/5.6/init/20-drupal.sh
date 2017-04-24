87345e6a3903be440e3cdaae59a0f91f8822cce3
Chingis Sandanov <chingis@wodby.com>
2017-04-24 18:42:10 +0700

    add env var to force init

--- 6/5.6/init/20-drupal.sh
+++ 6/5.6/init/20-drupal.sh
@@ -6,6 +6,6 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-if [[ -z "${DRUPAL_VERSION}" && -z "${DRUPAL_SKIP_INIT}" ]]; then
+if [[ -z "${DRUPAL_VERSION}" || -n "${DRUPAL_AUTO_INIT}" ]]; then
     su-exec www-data make init -f /usr/local/bin/actions.mk
 fi
