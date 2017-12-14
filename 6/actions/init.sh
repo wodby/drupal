803ac7012cb0bfedf2f0bf900ecca92f07404279
Chingis Sandanov <chingis@wodby.com>
2017-12-14 11:48:40 +0700

    Fix file existence conditions

--- 6/actions/init.sh
+++ 6/actions/init.sh
@@ -6,7 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-if [[ ! -e "${APP_ROOT}/web/index.php" ]]; then
+if [[ ! -f "${APP_ROOT}/web/index.php" ]]; then
     echo >&2 "Drupal not found in ${APP_ROOT} - copying now..."
     rsync -rlt "/usr/src/drupal/" "${APP_ROOT}/web"
     echo >&2 "Complete! Drupal has been successfully copied to ${APP_ROOT}"
