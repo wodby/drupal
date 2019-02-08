6d111d83a6edd5104cbeb42b0736d9c36e4be1c0
Chingis Sandanov <chingis@wodby.com>
2019-02-08 21:56:36 +0700

    Update Drupal tests

--- 8/tests/run.sh
+++ 8/tests/run.sh
@@ -13,5 +13,5 @@ trap "docker rm -vf ${cid} > /dev/null" EXIT
 
 docker exec "${cid}" make init -f /usr/local/bin/actions.mk
 echo -n "Checking Drupal version... "
-docker exec "${cid}" drush -r "/var/www/html/web" status | grep -q 'Drupal version *: *8\.'
+docker exec "${cid}" drush -r "/var/www/html/web" status | grep -q 'Drupal root'
 echo "OK"
